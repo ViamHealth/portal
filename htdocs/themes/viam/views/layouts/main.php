c676b2d0682995ad2fa091e9547212bf031bebd6
kunalrvm <kunal.rachhoya@viamhealth.com>
2013-07-04 02:32:13 +0530

    temporary responsive layout to test whole page

--- htdocs/themes/viam/views/layouts/main.php
+++ htdocs/themes/viam/views/layouts/main.php
@@ -13,64 +13,46 @@
 </head>
 
 <body>
-	<div id="nrow"><div class="container">&nbsp;</div></div>		
-	<div id="header" class="outer">
-		<div class="inner">
-			<div class="logo"><h1><a href="#">Viam Health</a></h1></div>
-            <div class="u_col">
-				<div class="u_box clearfix">
-                		<?php if(Yii::app()->user->getIsGuest()==1) {?>
-                		<?php } else { ?>
-						<?php $this->widget('bootstrap.widgets.TbButtonGroup', array(
-					        'type'=>'link', // '', 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
-					        'buttons'=>array(
-					            array('label'=>Yii::app()->user->name, 'block'=>true, 'htmlOptions'=>array('class'=>'ub_inr')),
-					            array('items'=>array(
-					                array('label'=>'Profile Details', 'url'=>'#'),
-					                array('label'=>'Account Settings', 'url'=>'#'),
-					                array('label'=>'Privacy Settings', 'url'=>'#'),
-					                '---',
-					                array('label'=>'Logout', 'url'=>array("/user/logout")),
-					            )),
-					        ),
-					    )); ?>                        
-
-
-<!--                        <div class="u_img"><a href="#"><img width="25" height="25" alt="User Image" src="<?php echo Yii::app()->request->baseUrl; ?>images/thumb/s_img1.jpg"></a></div>
-                        <div class="u_nam"><a href="#"><?php echo (Yii::app()->user->name); ?></a></div>
-                        <div class="dd_lnks pp_box">
-                            <div class="twrap">&nbsp;</div>
-                            <div class="mwrap">
-                                <ul>
-                                </ul>
-                            </div>
-                            <div class="bwrap">&nbsp;</div>-->
-                        <?php } ?>
-                </div>
-            </div>
-			<div class="navigation">
-				<?php if(Yii::app()->user->getIsGuest()!=1) {?>
-				<?php $this->widget('bootstrap.widgets.TbNavbar',array(
-				    'items'=>array(
-				  		
-				        array(
-				            'class'=>'bootstrap.widgets.TbMenu',
-				            'items'=>array(
-				                array('label'=>'Home', 'url'=>array('/site/index')),
-				                array('label'=>'Journals', 'url'=>array('/site/page', 'view'=>'about')),
-				                array('label'=>'Files', 'url'=>array('/viam/healthfiles')),
-				                array('label'=>'Reminders', 'url'=>array('/site/contact')),
-				                //array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				                //array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-				            ),
-				        ),
-				    ),
-				)); ?>
-				<?php } ?>
-			</div>
+	<?php $this->widget('bootstrap.widgets.TbNavbar', array(
+		'type'=>'inverse', // null or 'inverse'
+    'brand'=>'Viam Health',
+    'brandUrl'=>'#',
+    'collapse'=>true, // requires bootstrap-responsive.css
+    'items'=>array(
+    	array(
+        'class'=>'bootstrap.widgets.TbMenu',
+        'items'=>array(
+            array('label'=>'Viam Health', 'url'=>array('/site/index')),
+            array('label'=>'Journals', 'url'=>array('/site/page', 'view'=>'about')),
+            array('label'=>'Files', 'url'=>array('/viam/healthfiles')),
+            array('label'=>'Reminders', 'url'=>array('/site/contact')),
+            //array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+            //array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+        ),
+    	),
+    	array(
+        'class'=>'bootstrap.widgets.TbMenu',
+        'htmlOptions'=>array('class'=>'pull-right'),
+        'items'=>array(
+	    		array('label'=>Yii::app()->user->name, 'url'=>'#', 'items'=>array(
+		       array('label'=>'Profile Details', 'url'=>'#'),
+		       array('label'=>'Account Settings', 'url'=>'#'),
+		       array('label'=>'Privacy Settings', 'url'=>'#'),
+		       array('label'=>'Logout', 'url'=>array("/user/logout")),
+		      )
+		    ),
+      )),
+				
+				//	            array('label'=>Yii::app()->user->name, 'block'=>true, 'htmlOptions'=>array('class'=>'ub_inr')),
+			
+    ),
+	)); ?>
+				<?php //if(Yii::app()->user->getIsGuest()!=1) {?>
+				
 		</div>	
 	</div>
 	<div id="body">
+		<br/>
 		<?php echo $content; ?>
 	</div>
 	<!--<div class="footer">
