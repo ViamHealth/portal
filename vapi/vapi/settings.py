effe854d8a742b6dc7e8aabed47359ac62f48530
Kunal <kunal.rachhoya@viamhealth.com>
2013-07-21 21:32:44 +0530

    Added login for browesable apis and changed branding

--- vapi/vapi/settings.py
+++ vapi/vapi/settings.py
@@ -117,6 +117,7 @@ ROOT_URLCONF = 'vapi.urls'
 WSGI_APPLICATION = 'vapi.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/kunal/web/portal/vapi/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
