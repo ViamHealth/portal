f71ce8464595ebec9c54f3c524236d95078a8312
Kunal <kunal.rachhoya@viamhealth.com>
2013-07-22 21:51:15 +0530

    App only for logged in users

--- webapp/protected/components/Controller.php
+++ webapp/protected/components/Controller.php
@@ -20,4 +20,24 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+
+	public function beforeAction($action)
+    {
+        Yii::app()->user->setReturnUrl(Yii::app()->request->getUrl());
+        if (Yii::app()->user->isGuest ){
+        	if(($this->id == "site" && $action->id == "login")){
+        		//Login page
+        	}
+        	else{
+        		if(Yii::app()->request->isAjaxRequest){
+        			die("user not logged in");
+        			//Handle ajax call
+        		} else {
+                	$this->redirect(Yii::app()->createUrl('site/login'));
+            	}
+            }
+        }
+        //optionally include code here if its an authenticated user
+        return true;
+    }
 }
