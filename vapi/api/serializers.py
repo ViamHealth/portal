ee8e0148de14460a8e670e54e256afc428b0da5d
Kunal Rachhoya <kunal.rachhoya@viamhealth.com>
2013-10-17 09:59:33 +0530

    calory spent formaule

--- vapi/api/serializers.py
+++ vapi/api/serializers.py
@@ -115,9 +115,19 @@ class PhysicalActivitySerializer(serializers.ModelSerializer):
 class UserPhysicalActivitySerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.Field(source='user.id')
     physical_activity = PhysicalActivitySerializer(many=False)
+    calories_spent = serializers.SerializerMethodField('get_calories_spent')
     class Meta:
         model = UserPhysicalActivity
-        fields = ('id','user','weight','time_spent','physical_activity','user_calories_spent')
+        fields = ('id','user','weight','time_spent','physical_activity','calories_spent')
+
+    def get_calories_spent(self, obj=None):
+        if obj is not None:
+            if obj.user_calories_spent is not None:
+                return obj.user_calories_spent
+            elif obj.weight is not None and obj.time_spent is not None and obj.physical_activity is not None:
+                return ( float(obj.weight) * float(obj.time_spent) * obj.physical_activity.value * 2.2 ) / 60 
+        else:
+            return None
 
 
 class UserPhysicalActivityCreateSerializer(serializers.HyperlinkedModelSerializer):
