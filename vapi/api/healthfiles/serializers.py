8c10c5d1ce0044faa65408a1736666c323fccb0e
Kunal Rachhoya <kunal.rachhoya@viamhealth.com>
2013-12-03 22:07:35 +0530

    adding username to reminders

--- vapi/api/healthfiles/serializers.py
+++ vapi/api/healthfiles/serializers.py
@@ -1,4 +1,4 @@
-
+    
 from .models import *
 from rest_framework import serializers
 from api.serializers_helper import *
@@ -21,10 +21,11 @@ class HealthfileSerializer(serializers.HyperlinkedModelSerializer):
     download_url = serializers.Field(source='download_url')
     updated_by = serializers.Field(source='user.id')
     updated_at = serializers.SerializerMethodField('get_updated_at')
+    updated_by_username = serializers.Field(source='user.first_name')
     #tags = HealthfileTagListingField(many=True)
     class Meta:
         model = Healthfile
-        fields = ('id', 'user','name' ,'description','mime_type','download_url','updated_by','updated_at')
+        fields = ('id', 'user','name' ,'description','mime_type','download_url','updated_by','updated_at','updated_by_username',)
     def get_updated_at(self,obj=None):
         return calendar.timegm(obj.updated_at.timetuple())
 
