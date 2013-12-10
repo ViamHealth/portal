4e9620351e5f60a36e96750c26f98d0aa8511eae
Kunal Rachhoya <kunal.rachhoya@viamhealth.com>
2013-12-10 19:37:45 +0530

    default as json for apis

--- vapi/vapi/settings.py
+++ vapi/vapi/settings.py
@@ -209,6 +209,9 @@ AUTHENTICATION_BACKENDS = (
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
